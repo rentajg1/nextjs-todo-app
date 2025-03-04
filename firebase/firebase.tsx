fc5fdbeec20583a41207c9a699f663cf619e08b1
rentajg1 <respect1125@icloud.com>
2025-03-05 08:07:05 +0900

    databaseの接続

---
+++ firebase/firebase.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
+import { GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAE2wlCIONHwyDOSFrMv7UDWZPAZbhIVz4',
+  authDomain: 'nextjs-todo-app-9d8b3.firebaseapp.com',
+  projectId: 'nextjs-todo-app-9d8b3',
+  storageBucket: 'nextjs-todo-app-9d8b3.firebasestorage.app',
+  messagingSenderId: '295768016413',
+  appId: '1:295768016413:web:6693a6f44b359c2d4828f4',
+  measurementId: 'G-R07MYD9N99',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)ß
+
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+
+export { auth, provider }
